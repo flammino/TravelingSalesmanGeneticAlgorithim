4dcbc632a07b159fdf3ccb0fe1a8f9339a2a4b65
Adam Flammino <flammino@gmail.com>
2017-06-29 10:58:55 -0400

    Random and specific city generation, distance between cities

--- TravelingSalesmanGeneticAlgorithim.cpp
+++ TravelingSalesmanGeneticAlgorithim.cpp
@@ -1,11 +1,63 @@
-// TravelingSalesmanGeneticAlgorithim.cpp : Defines the entry point for the console application.
-//
+// TravelingSalesmanGeneticAlgorithim.cpp 
+// Adam Flammino 6/28
+// Uses a genetic algorithm to solve the traveling salesman problem
 
 #include "stdafx.h"
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <time.h>
+
+class City {
+private:
+	int x;
+	int y;
+public:
+	// Constructor with random city coordinatates in a 1000 unit grid
+	City() {
+		x = rand() % 1000;
+		y = rand() % 1000;
+	}
+	// Constructor with specific city coordinates
+	City(int lat, int lon) {
+		x = lat;
+		y = lon;
+	}
+	// Get x coordinat;
+	int getX() {
+		return x;
+	}
+	// Get y coordinate
+	int getY() {
+		return y;
+	}
+	// Distance between cities
+	double distance(City city1, City city2) {
+		int latDist = abs(city1.getX() - city2.getX());
+		int lonDist = abs(city1.getY() - city2.getY());
+		return sqrt((latDist * latDist) + (lonDist *lonDist)); // a^2 + b^2 = c^2
+	}
+};
 
 
 int main()
 {
+	srand(time(NULL)); // Needed so random cities are actually random
+	City c;
+	int totalDist = 0;
+	std::vector<City> cities;
+	for (int i = 0; i < 50; i++) { // creates 50 random cities
+		cities.push_back(City());
+	}
+	for (int i = 0; i < cities.size(); i++) {
+		std::cout << "The coordinates for city " << i + 1 << " are\t" << cities[i].getX() << ", " << cities[i].getY() << std::endl;
+		if (i < cities.size() - 1) {
+			totalDist += c.distance(cities[i], cities[i + 1]);
+		}
+	}
+	std::cout << "Visiting cities in order, the total distance traveled is " << totalDist << std::endl;
     return 0;
 }
 
